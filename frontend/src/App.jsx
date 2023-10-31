7216ee126e267ecfaea9a5b2791b8021991df436
กีตาร์เอาเอง <45527907+dudkinox@users.noreply.github.com>
2023-10-31 13:25:16 +0700

    feat (no-card) : :guitar: add socket io setup repo

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ul id="messages" />
+        <form id="form" action>
+          <input id="input" autoComplete="off" />
+          <button>Send</button>
+        </form>
+      </div>
+    </>
+  );
+}
+
+export default App;
